8d1091c63899d748bd762e55d17d8f1479607539
Renan-Silva235 <renan.rubio94@gmail.com>
2026-04-18 00:20:29 -0300

    Creating a controller class to manage donations.

--- src/main/java/com/colaborativos_gestao_sistema_api/controllers/DonationManagerController.java
+++ src/main/java/com/colaborativos_gestao_sistema_api/controllers/DonationManagerController.java
@@ -1,4 +1,50 @@
 package com.colaborativos_gestao_sistema_api.controllers;
 
+import com.colaborativos_gestao_sistema_api.services.DonationManagerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/donation")
 public class DonationManagerController {
+
+    @Autowired
+    private DonationManagerService donationManagerService;
+
+    @PostMapping("/store")
+    public ResponseEntity<String> store(@RequestBody @Valid Object donation){
+        try{
+            donationManagerService.processDonation(donation);
+            return ResponseEntity.ok("Doação cadastrada com sucesso.");
+        }catch (Exception error){
+            return ResponseEntity.badRequest().body("Erro ao cadastrar doação: " + error.getMessage());
+        }
+    }
+
+    @GetMapping("/index")
+    public ResponseEntity<?> index(){
+        return ResponseEntity.ok(donationManagerService.listAllProducts());
+    }
+
+    @GetMapping("/show")
+    public ResponseEntity<List<Object>> show(@RequestParam String name){
+        List<Object> results = donationManagerService.searchProductByName(name);
+        return ResponseEntity.ok(results);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Object updateData){
+        try {
+            Object updated =  donationManagerService.updateProduct(id, updateData);
+            return ResponseEntity.ok(updated);
+        }catch (RuntimeException error){
+            return ResponseEntity.status(404).body(error.getMessage());
+        }catch (Exception error){
+            return ResponseEntity.badRequest().body("Erro ao atualizar" + error.getMessage());
+        }
+    }
 }
