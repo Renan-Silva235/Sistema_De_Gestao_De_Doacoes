a0f0cf6aac55c7b49f092520a4b7f15f90eb87a1
Renan-Silva235 <renan.rubio94@gmail.com>
2026-04-26 00:37:42 -0300

    Response class update

--- src/main/java/com/colaborativos_gestao_sistema_api/DTOs/LoginResponseDTO.java
+++ src/main/java/com/colaborativos_gestao_sistema_api/DTOs/LoginResponseDTO.java
@@ -1,5 +1,4 @@
 package com.colaborativos_gestao_sistema_api.DTOs;
 
-import com.colaborativos_gestao_sistema_api.enums.Roles;
 
 public record LoginResponseDTO(String username, String role, String token) {}
